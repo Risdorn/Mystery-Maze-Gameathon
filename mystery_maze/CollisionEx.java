ef1505b2b6ee03cdbb436cfa6ee2e46a920aeef3
Risdorn <rishabhindoria2203@gmail.com>
2024-06-26 20:37:18 +0530

    Learnt Animation, Moving Sprites, Shooting and Collision Detection in Java Added images for short projects Next is to practice making few basic games and then start with Mystery Maze

--- mystery_maze/CollisionEx.java
+++ mystery_maze/CollisionEx.java
@@ -3,28 +3,29 @@ package mystery_maze;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
-public class Donut extends JFrame {
-    
-    public Donut() {
+public class CollisionEx extends JFrame {
 
+    public CollisionEx() {
+        
         initUI();
     }
-
-    private void initUI() {
-
-        add(new Board());
-
-        setSize(330, 330);
-
-        setTitle("Donut");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-    }    
     
+    private void initUI() {
+        
+        add(new Board());
+        
+        setResizable(false);
+        pack();
+        
+        setTitle("Collision");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
     public static void main(String[] args) {
         
         EventQueue.invokeLater(() -> {
-            Donut ex = new Donut();
+            CollisionEx ex = new CollisionEx();
             ex.setVisible(true);
         });
     }
